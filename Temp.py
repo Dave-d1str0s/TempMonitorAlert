a1735d760edf47ee307fe4d1e610340d5bdf7a3e
dave_d1str0s <71856444+Dave-d1str0s@users.noreply.github.com>
2020-09-24 19:32:06 -0500

    Update Temp.py

--- Temp.py
+++ Temp.py
@@ -1,6 +1,6 @@
 # Temp-Monitor-and-Alert
 #Temperature monitor I setup for a datacenter to remove the need for manual monitoring or expensive solutions.
-#I used a Raspberry Pi, a DHT22 sensor, and python to monitor and alert via email.
+#I used a Raspberry Pi, a DHT22 sensor, and python to monitor and alert via email. Once email is sent, it stops.
 
 import Adafruit_DHT
 import time
